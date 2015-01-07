06f3e3f6ca30f4f53a4da066425943d4546754fe
Nikhil <nikhil.sharma.ece@gmail.com>
2015-01-07 22:16:40 +0530

    customer model

--- service/src/main/java/com/agamin/model/Customer.java
+++ service/src/main/java/com/agamin/model/Customer.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 
 public class Customer implements Serializable{
 
-	//Serialize
+	
 	private static final long serialVersionUID = 1L;
 	private Double customerid;
 	private String city;
