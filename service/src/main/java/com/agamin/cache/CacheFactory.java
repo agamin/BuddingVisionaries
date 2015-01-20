0a642196d3ccf8cddc09da1d23eec4e37539a030
Nikhil <nikhil.sharma.ece@gmail.com>
2015-01-20 21:46:20 +0530

    deleting MenuModel

--- service/src/main/java/com/agamin/cache/CacheFactory.java
+++ service/src/main/java/com/agamin/cache/CacheFactory.java
@@ -7,7 +7,7 @@ import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
 import org.springframework.stereotype.Component;
 
-import com.agamin.model.MenuModel;
+import com.agamin.model.Menu;
 
 @Component
 public class CacheFactory {
@@ -17,12 +17,12 @@ public class CacheFactory {
 	@Autowired
 	private CacheManager cacheManager;
 	
-	private AgaminCache<String, Map<String, MenuModel>> menuCache;
+	private AgaminCache<String, Map<String, Menu>> menuCache;
 	
-	public AgaminCache<String, Map<String, MenuModel>> getMenuCache() {
+	public AgaminCache<String, Map<String, Menu>> getMenuCache() {
 		if(menuCache == null) {
 			Cache cache = cacheManager.getCache(MENU_CACHE_NAME);
-			menuCache = new AgaminCache<String, Map<String,MenuModel>>(cache);
+			menuCache = new AgaminCache<String, Map<String,Menu>>(cache);
 		}
 		return menuCache;
 	}
